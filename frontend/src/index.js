2fb6c809a534a8d8e6480673b32d0047baa0bd4e
jyothivallabhu <v.jyothi55@gmail.com>
2023-09-09 15:04:25 +0530

    frontend changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer'
+import Error from './components/Error';
+import Login from './components/Login';
+import App from './components/App'
+
+
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+
+
 import reportWebVitals from './reportWebVitals';
+
+const AppLayout = () =>{
+  return(
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+
+}
+
+const appRouter = createBrowserRouter([
+  {
+    "path" : "/",
+    "element" : <AppLayout />,
+    "errorElement" : <Error />,
+    children : [
+      {
+        "path": "/",
+        "element": <App />
+
+      },
+      {
+        "path" : "Login",
+        "element" : <Login />
+      }
+    ]
+
+  }
+
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={appRouter} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ 
 reportWebVitals();
